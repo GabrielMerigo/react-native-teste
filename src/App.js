f17490c702acaff9055951faa99aae4afffe5d34
gabriel.merigo <gabriel.merigo.dev@gmail.com>
2022-05-26 17:39:04 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react"; // Sempre quando tivermos JSX em nosso código nós precisamos ter essa variavel React
+import { Text } from "react-native";
+
+export default function App(){
+    return <Text>Primeiro Componente</Text>
+}
