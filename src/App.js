bc6a60018ea2778e896bd921ccb2cfe393e12295
gabriel.merigo <gabriel.merigo.dev@gmail.com>
2022-05-27 06:10:37 -0300

    first component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react"; // Sempre quando tivermos JSX em nosso código nós precisamos ter essa variavel React
 import { Text } from "react-native";
 
+
+import FirstComponent from '../src/components/first.js'
+
 export default function App(){
-    return <Text>Primeiro Componente</Text>
+    return <FirstComponent />
 }
---
+++ src/components/first.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native'
+
+const App = () => {
+    return(
+        <Text>First!!</Text>
+    )
+} 
+
+export default App
